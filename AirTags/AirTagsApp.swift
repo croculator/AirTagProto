178c2a4894308a39491d863d6fb87741e6860d34
michael <--get>
2021-06-17 11:00:37 -0700

    init

---
+++ AirTags/AirTagsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirTagsApp.swift
+//  AirTags
+//
+//  Created by Michael Diaz on 4/21/21.
+//
+
+import SwiftUI
+
+@main
+struct AirTagsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
